525376331627a2fdaf68849a5ddc76c59beb4494
Eragonis <starkermeister@outlook.com>
2025-09-16 11:43:08 +0200

    two new Class[Article, Warehouse]

---
+++ src/main/scala/Article.scala
@@ -0,0 +1,1 @@
+class Article(var id: Int, var name: String, var price: Double, var comment: String)
---
+++ src/main/scala/Warehouse.scala
@@ -0,0 +1,1 @@
+class Warehouse(var id: Int, var location: String, var name: String, var Artcle_id: Int)
