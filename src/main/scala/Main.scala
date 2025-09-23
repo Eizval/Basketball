e58d6b1211c2c007058fdd9ad4888c3a9ef3445b
Eizval <damian.mermod@mac.com>
2025-09-23 10:24:45 +0200

    created Main class, added important Base Structure

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,63 @@
+import scala.io.StdIn.readLine
+
+@main def main(): Unit = {
+  var done = false
+  while !done do {
+    try
+      println("Enter: \n 't' for Teams \n 'm' for Matches \n 'p' for player \n 'v' for Verein \n 'l' for Lager \n 'a' for Artikel \n 'q' to quit")
+
+      val input = readLine().charAt(0).toLower
+      input match
+        case 'q' => done = finish(done)
+        case 't' => teamsView()
+        case 'm' => match_history()
+        case 'p' => player()
+        case 'v' => verein()
+        case 'l' => lager()
+        case 'a' => artikel()
+
+    catch
+        {
+          case e: Exception =>
+           println(s"Es ist ein Fehler aufgetreten: ${e.getMessage}")
+           println("Stelle sicher das du die richtigen Eingabewerte eingiebst")
+    }
+  }
+}
+
+def teamsView(): Unit = {
+  println("teams")
+}
+
+
+def match_history(): Unit = {
+  println("match")
+}
+
+
+def player(): Unit = {
+
+}
+
+
+def verein(): Unit = {
+  println("verein")
+}
+
+
+def lager(): Unit = {
+  println("lager")
+}
+
+
+def artikel(): Unit = {
+  println("artikel")
+}
+
+
+def finish(done: Boolean): Boolean = {
+  println("Good Bye")
+  true
+}
+
+
--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,3 +1,19 @@
-class Player (var id: Int, var name: String, var playerNumber: Int, var age: Int, var playerType: List[String]){
+class Player (var id: Int, var name: String, var playerNumber: Int, var age: Int, var playerType: List[String]) {
+
+
+}
+
+
+def getPlayer(): Unit = {
+
+}
+
+
+def editPlayer(): Unit = {
+
+}
+
+
+def deletePlayer(): Unit = {
 
 }
