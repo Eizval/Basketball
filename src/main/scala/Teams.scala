cbd96e374f1e0697f13d4962cb2f25e747275c63
Eizval <damian.mermod@mac.com>
2025-09-30 11:23:27 +0200

    Made a getTeams and getAllTeams function

--- src/main/scala/Teams.scala
+++ src/main/scala/Teams.scala
@@ -1,3 +1,29 @@
-class Teams (val id: Int, val name: String, val teammates: List[Int]){
+case class Team (val id: Int, val name: String, val teammates: List[Int]){
+  override def toString: String =
+    s"Teams(id = $id, name = $name, teammates = ${teammates.mkString(", ")})"
+}
+
+val team1 = new Team(1, "KTV", List(13))
+val team2 = new Team(1, "KTV", List(13,27))
+val teams: List[Team] = List(team1,team2)
+
+
+def getTeams(id: Int): List[Team] = {
+  teams.filter(p => p.id == id)
+}
+
+def getAllTeams(): List[Team] = {
+  teams
+}
+
+def editTeams(): Unit = {
+
+}
+
+def deleteTeams(): Unit = {
+
+}
+
+def addTeams(): Unit = {
 
 }
